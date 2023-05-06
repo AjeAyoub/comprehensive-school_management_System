2eb30ef984714d241a55f65fec25f68a5ac71bd9
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-05-06 13:04:59 +0200

    configurate theme

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/empty', function () {
+    return view('empty');
+})->middleware(['auth', 'verified'])->name('empty');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
